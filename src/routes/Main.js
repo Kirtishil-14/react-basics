f43ee7cc03e6bd5370dd164b4ad1a46a79bcbbc7
Kirtishil-14 <kirtishilpatil14@gmail.com>
2022-05-03 12:15:41 +0530

    .

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
   const navigate = useNavigate();
   return (
     <>
-      <button onClick={() => navigate('HooksOld')}>Hooks 1</button>
-      <button onClick={() => navigate('Hooks')}>Hooks 2</button>
-      <button onClick={() => navigate('Todo')}>Todo</button>
-      <button onClick={() => navigate('Menu')}>Menu</button>
-      <button onClick={() => navigate('Form')}>Form</button>
+      <button onClick={() => navigate("HooksOld")}>Hooks 1</button>
+      <button onClick={() => navigate("Hooks")}>Hooks 2</button>
+      <button onClick={() => navigate("Todo")}>Todo</button>
+      <button onClick={() => navigate("Menu")}>Menu</button>
+      <button onClick={() => navigate("Form")}>Form</button>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
