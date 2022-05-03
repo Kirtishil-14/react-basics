32b3a32e33f8a1b096f2da140892501d97604e61
Kirtishil-14 <kirtishilpatil14@gmail.com>
2022-05-03 12:09:10 +0530

    done

--- src/routes/HooksOld.jsx
+++ src/routes/HooksOld.jsx
@@ -1,22 +1,26 @@
-import React, { useState, useEffect, useReducer, createContext, useContext } from 'react'
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  createContext,
+  useContext,
+} from "react";
 
 const UserContext = createContext();
 
 const reducer = (state, action) => {
-  if (action.type === 'Add') {
+  if (action.type === "Add") {
     state = state + 1;
-  } else if (state > 0 && action.type === 'Sub') {
+  } else if (state > 0 && action.type === "Sub") {
     state = state - 1;
   }
   return state;
-}
+};
 
 const Hooks = () => {
-
   const [count, setCount] = useState(0);
 
-  const [state, dispatch] = useReducer(reducer, 10)
-
+  const [state, dispatch] = useReducer(reducer, 10);
 
   useEffect(() => {
     console.log(`you clicked ${count} times`);
@@ -25,22 +29,38 @@ const Hooks = () => {
   return (
     <>
       <p>{state}</p>
-      <button type="button" className="btn btn-primary m-1" onClick={() => dispatch({ type: "Add" })}>Reducer Add</button>
-      <button type="button" className="btn btn-primary m-1" onClick={() => dispatch({ type: "Sub" })}>Reducer Sub</button>
+      <button
+        type="button"
+        className="btn btn-primary m-1"
+        onClick={() => dispatch({ type: "Add" })}
+      >
+        Reducer Add
+      </button>
+      <button
+        type="button"
+        className="btn btn-primary m-1"
+        onClick={() => dispatch({ type: "Sub" })}
+      >
+        Reducer Sub
+      </button>
       <UserContext.Provider value={count}>
         <p>{count}</p>
-        <button type="button" className="btn btn-primary" onClick={() => setCount(count + 1)}>Add</button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => setCount(count + 1)}
+        >
+          Add
+        </button>
         <ChildComponent count={count} />
       </UserContext.Provider>
     </>
-  )
-}
+  );
+};
 
 const ChildComponent = () => {
   const count = useContext(UserContext);
-  return (
-    <p>{count}</p>
-  )
-}
+  return <p>{count}</p>;
+};
 
-export default Hooks
+export default Hooks;
