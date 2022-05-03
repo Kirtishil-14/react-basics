9578da09f09eee19d60e93ca685608d1e1ff45b7
Kirtishil-14 <kirtishilpatil14@gmail.com>
2022-05-03 12:10:16 +0530

    .

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Main from './Main';
-import Hooks from './Hooks';
-import Todo from '../todo/Todo';
-import Menu from '../menu-cart/Menu';
-import Form from '../form/Form';
-import HooksOld from './HooksOld';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./Main";
+import Hooks from "./Hooks";
+import Todo from "../todo/Todo";
+import Menu from "../menu-cart/Menu";
+import Form from "../form/Form";
+import HooksOld from "./HooksOld";
 
 const Home = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />}></Route>
+        <Route path="/HooksOld" element={<HooksOld />}></Route>
         <Route path="/Hooks" element={<Hooks />}></Route>
         <Route path="/Todo" element={<Todo />}></Route>
         <Route path="/Menu" element={<Menu />}></Route>
         <Route path="/Form" element={<Form />}></Route>
-        <Route path="/HooksOld" element={<HooksOld />}></Route>
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
