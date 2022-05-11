276c9dc98b2c456368649b3417ad1952ba9e4c25
Kirtishil-14 <kirtishilpatil14@gmail.com>
2022-05-11 11:15:38 +0530

    category

--- src/menu-cart/Menu.jsx
+++ src/menu-cart/Menu.jsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import MenuApi from './MenuApi'
+import React, { useState, useEffect } from "react";
+import MenuApi from "./MenuApi";
 
 const uniqueList = [
-  ...new Set(MenuApi.map((elem) => {
-    return elem.category
-  })
-  ), "All"
-]
+  ...new Set(
+    MenuApi.map((elem) => {
+      return elem.category;
+    })
+  ),
+  "All",
+];
 
 const Menu = () => {
-
   const [menuData, setMenuData] = useState(MenuApi);
 
   const [menuList, setMenuList] = useState(uniqueList);
 
   const filterItem = (category) => {
-    if (category === 'All') {
+    if (category === "All") {
       setMenuData(MenuApi);
       return;
     }
@@ -23,38 +24,45 @@ const Menu = () => {
       return elem.category === category;
     });
     setMenuData(updateList);
-  }
+  };
 
   return (
     <>
       <nav className="navbar">
         <div>
-          {
-            menuList.map((elem, i) => {
-              return (
-                <button key={i} type="button" className="btn btn-primary m-1" onClick={() => filterItem(elem)}>{elem}</button>
-              )
-            })
-          }
+          {menuList.map((elem, i) => {
+            return (
+              <button
+                key={i}
+                type="button"
+                className="btn btn-primary m-1"
+                onClick={() => filterItem(elem)}
+              >
+                {elem}
+              </button>
+            );
+          })}
         </div>
       </nav>
       <div className="container d-flex">
-        {
-          menuData.map((menu) => {
-            const { id, name, category, price, description } = menu;
-            return (
-              <div className="card  m-2" style={{ width: '18rem' }} key={id}>
-                <div className="card-body">
-                  <h5 className="card-title">{name} - {price}</h5>
-                  <p className="card-text">{description}</p>
-                </div>
+        {menuData.map((menu) => {
+          const { id, name, category, price, description } = menu;
+          return (
+            <div className="card  m-2" style={{ width: "18rem" }} key={id}>
+              <div className="card-body">
+                <h5 className="card-title">
+                  {name} - {price}
+                </h5>
+                <p className="card-text">
+                  {category} {description}
+                </p>
               </div>
-            )
-          })
-        }
+            </div>
+          );
+        })}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Menu;
